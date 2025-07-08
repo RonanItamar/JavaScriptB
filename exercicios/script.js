731ea10b91d30bb75bb7414bd31efb910ec706ed
RonanItamar <ronan.filho2009@gmail.com>
2025-07-08 12:16:35 -0300

    08/07 Adicionado Lista de Ex

---
+++ exercicios/script.js
@@ -0,0 +1,7 @@
+//1 - Soma simples
+function somar(){
+    let n1 = Number(document.getElementById("num1").value);
+    let n2 = Number(document.getElementById("num2").value);
+    document.getElementById("resultado1").innerText = "Resultado : " + (n1 + n2);
+
+}
