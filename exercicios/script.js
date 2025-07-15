d999cf6881b8b56df4ddace70146efd1b3cd2e19
RonanItamar <ronan.filho2009@gmail.com>
2025-07-15 12:22:56 -0300

    Aula dia 15/07

--- exercicios/script.js
+++ exercicios/script.js
@@ -1,7 +1,74 @@
 //1 - Soma simples
-function somar(){
+function somar() {
     let n1 = Number(document.getElementById("num1").value);
     let n2 = Number(document.getElementById("num2").value);
     document.getElementById("resultado1").innerText = "Resultado : " + (n1 + n2);
+
+}
+
+function idade() {
+
+    let idade = Number(document.getElementById("idade").value);
+
+    if (idade >= 18) {
+        document.getElementById("resultado2").innerText = "Maior de Idade";
+
+    } else {
+        document.getElementById("resultado2").innerText = "Menor de Idade";
+
+    }
+}
+
+function contar(){
+
+
+}
+
+function verificarpar(){
+
+
+}
+
+function nomecaixaalta(){
+
+
+}
+
+function maior(){
+
+
+}
+
+function tabuada(){
+
+}
+
+function contar(){
+
+
+}
+
+function medianotas(){
+
+
+}
+
+function conversor(){
+
+
+}
+
+function diasemana(){
+
+
+}
+
+function senha(){
+
+
+}
+
+function contagem(){
+
 
 }
